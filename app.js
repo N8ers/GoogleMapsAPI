2b20394f9f8b7f696dc0a043576f0148e11f437f
N8ers <nathanraysheryak@gmail.com>
2018-05-23 10:30:37 -0400

    gotta go!

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("sup bruh");
+// alert("sup bruh");
+
+
+function initMap(){
+    let options = {
+        zoom: 8,
+        center:{lat: 39.7684, lng: -86.1581}
+    }
+    let map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+
+// let location() {
+//     let 
+// }
+
+GET `https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters`;
